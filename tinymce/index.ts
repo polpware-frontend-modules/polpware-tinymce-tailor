1873774d6749333db715c03aa24d43998db0c129
Tomcat <polpware@gmail.com>
2025-09-16 23:02:17 -0400

    Fix build script

--- tinymce/index.ts
+++ tinymce/index.ts
@@ -2,14 +2,14 @@
 
 export * from './api/Env';
 export * from './api/html/Entities';
-export * from './api/util/EventDispatcher.ts';
-export * from './api/util/FakeStorage.ts';
-export * from './api/util/I18n.ts';
-export * from './api/util/LocalStorage.ts';
-export * from './api/util/Observable.ts';
-export * from './api/util/Tools.ts';
-export * from './api/util/URI.ts';
-export * from './api/util/VK.ts';
+export * from './api/util/EventDispatcher';
+export * from './api/util/FakeStorage';
+export * from './api/util/I18n';
+export * from './api/util/LocalStorage';
+export * from './api/util/Observable';
+export * from './api/util/Tools';
+export * from './api/util/URI';
+export * from './api/util/VK';
 
 export * from './events/EventUtils';
 export * from './util/ArrUtils';
