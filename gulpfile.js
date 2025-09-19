e9835abaa66c6542a5399ccf6b9731a3fc6e9e8d
Tomcat <polpware@gmail.com>
2025-09-19 14:27:36 -0400

    Add version control in the toolchain

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const { src, dest, parallel } = require('gulp');
+const bumpversion = require('gulp-bump');
+
+// `fs` is used instead of require to prevent caching in watch (require caches)
+const fs = require('fs');
+
+function getVersion() {
+    return fs.readFileSync('./VERSION', 'utf8', function(err, data) {
+        return data;
+    });
+};
+
+function bump() {
+
+    const newVer = getVersion().trim();
+
+// bump versions on package/bower/manifest
+    return src(['./package.json'])
+        .pipe(bumpversion({
+            version: newVer
+        }))
+        .pipe(dest(function(x) {
+            return x.base;
+        }));
+}
+
+exports.bump = bump;
+exports.default = bump;
